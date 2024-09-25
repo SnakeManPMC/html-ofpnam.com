425bdfa236f7b6a7d4f9c6f80d18388debf887ce
Snake Man <snakeman@phnet.fi>
2024-09-25 10:23:33 +0000

    my first commit, yippy

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Operation Flashpoint Vietnam War Modification</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Operation Flashpoint Vietnam War Modification">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("/var/www/pmctactical.org/include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+<h1>Operation Flashpoint Vietnam War Modification</h1>
+<p>Welcome to the best Vietnam War Modification for Personal Computers.</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+	<h2>Operation Flashpoint Vietnam</h2>
+
+<p>
+Welcome to OFPNAM.com all things about Operation Flashpoint (OFP) Vietnam (NAM).
+</p>
+
+<p>
+Vietnam: The Experience (VTE) is leading, most detailed and comprehensive vietnam mod for PC.
+</p>
+
+<p>
+ofp/arma vietnam war modification with multiple missions and campaigns for single and multiplayer.
+</p>
+
+<p>
+Visit other ofp/arma web sites: <a href="https://www.pmctactical.org" target="_blank">www.pmctactical.org</a> and <a href="https://www.vtemod.com" target="_blank">www.vtemod.com</a>.
+</p>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p>Back to <a href="index.php">Operation Flashpoint Vietnam root page</a></p>
+<br><br>
+<p><i>PMC Operation Flashpoint Vietnam 2003 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
